586e55de9dfa653d1f91816ffbc28f9fc60b7ad8
Ankit <ankitGhosh456@gmail.com>
2022-10-24 20:33:02 +0530

    Gambler if won or lost 50% of stake, would resign for the day

--- src/com/codinclub/Gambling.java
+++ src/com/codinclub/Gambling.java
@@ -7,24 +7,29 @@ public class Gambling {
         System.out.println("------ Welcome to Gambling Simulator ------ ");
         Scanner scanner = new Scanner(System.in);
         System.out.println(" Please enter amount of stack ");
-        int stackPerDay = scanner.nextInt();
+        int stakePerDay = scanner.nextInt();
         System.out.println(" Enter the price for the game ");
         int betPrice = scanner.nextInt();
 
-        System.out.println(" Total stack per day is $ : " + stackPerDay);
+        int maxStack = stakePerDay + 50 % stakePerDay;
+        int minStack = stakePerDay - 50 % stakePerDay;
+
+        System.out.println(" Total stack per day is $ : " + stakePerDay);
         System.out.println(" Bet price per game is $ : " + betPrice);
 
-        int play = (int)(Math.random() * 2);
-
-        switch (play){
-            case 0:
-                stackPerDay += betPrice;
-                System.out.println("\n Player Won $1 ");
-                System.out.println("Stake after winning the game is $ " + stackPerDay);
-            case 1:
-                stackPerDay -= betPrice;
-                System.out.println("\n Player lost $1 ");
-                System.out.println("Stake after the losing the game is $ " + stackPerDay);
+        while (stakePerDay > minStack && stakePerDay < maxStack) {
+            int play = (int) (Math.random() * 2);
+            switch (play) {
+                case 0:
+                    stakePerDay += betPrice;
+                    System.out.println("\n Player Won $1 ");
+                    System.out.println("Stake after winning the game is $ " + stakePerDay);
+                case 1:
+                    stakePerDay -= betPrice;
+                    System.out.println("\n Player lost $1 ");
+                    System.out.println("Stake after the losing the game is $ " + stakePerDay);
+            }
         }
+        System.out.println("Player has reached the 50% stake limit and the stake now is $" + stakePerDay + " for the day.");
     }
 }
