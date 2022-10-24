0194fdf1d7d54fb5f11a6df89672ab627cc1d0f7
Ankit Ghosh <111551548+AnkitGhosh08@users.noreply.github.com>
2022-10-24 21:11:10 +0530

    Delete Main.java

--- src/com/codinclub/Main.java
+++
@@ -1,9 +0,0 @@
-package com.codinclub;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Gambling gambling = new Gambling();
-        gambling.gamble20Days();
-    }
-}
