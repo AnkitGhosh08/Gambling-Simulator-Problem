cbae905d9bb850aebe35b6e7c51d5d223324f88c
Ankit <ankitGhosh456@gmail.com>
2022-10-24 20:19:20 +0530

    Gambler make  bet so either win or loose

--- src/com/codinclub/Gambling.java
+++ src/com/codinclub/Gambling.java
@@ -11,7 +11,20 @@ public class Gambling {
         System.out.println(" Enter the price for the game ");
         int betPrice = scanner.nextInt();
 
-        System.out.println(" Total stack per day is : " + stackPerDay);
-        System.out.println(" Bet price per game is : " + betPrice);
+        System.out.println(" Total stack per day is $ : " + stackPerDay);
+        System.out.println(" Bet price per game is $ : " + betPrice);
+
+        int play = (int)(Math.random() * 2);
+
+        switch (play){
+            case 0:
+                stackPerDay += betPrice;
+                System.out.println("\n Player Won $1 ");
+                System.out.println("Stake after winning the game is $ " + stackPerDay);
+            case 1:
+                stackPerDay -= betPrice;
+                System.out.println("\n Player lost $1 ");
+                System.out.println("Stake after the losing the game is $ " + stackPerDay);
+        }
     }
 }
