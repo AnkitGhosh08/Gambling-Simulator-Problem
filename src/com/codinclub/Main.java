7f27f32c63d91baf9d02314e69ceb229979690a9
Ankit <ankitGhosh456@gmail.com>
2022-10-25 15:06:01 +0530

    If won then continue to play next month

--- src/com/codinclub/Main.java
+++ src/com/codinclub/Main.java
@@ -1,17 +1,12 @@
 package com.codinclub;
 
-import static com.codinclub.Gambling.*;
+import static com.codinclub.Gambling.gamble30Days;
+import static com.codinclub.Gambling.playNextMonth;
 
 public class Main {
     public static void main(String[] args) {
-        Gambling gamble = new Gambling();
-        gamble.gamble20Days();
-        System.out.println(" \n In total 30 days, the player has won " + win + " days and loose " + loose + " days");
-        System.out.println("The total gain amount for 30 Days is $" + profit); // printing the total profit
-        System.out.println("Maximum Amount Won: " + maxWin);
-        System.out.println("Maximum Amount Won On Day " + maxWinDay);
-        System.out.println("Maximum Amount Loss: " + maxLoss);
-        System.out.println("Maximum Amount Loss On Day " + maxLossDay);
+        gamble30Days();
+        playNextMonth();
     }
 }
 
