e2e9547c6d8dfb665043d94f1f97dac0b53a8141
Ankit <ankitGhosh456@gmail.com>
2022-10-25 14:54:07 +0530

    luckiest day where i won maxi or unluckiest day where i lost maxi.

--- src/com/codinclub/Main.java
+++ src/com/codinclub/Main.java
@@ -1,8 +1,17 @@
 package com.codinclub;
 
+import static com.codinclub.Gambling.*;
+
 public class Main {
     public static void main(String[] args) {
-        Gambling gambling = new Gambling();
-        gambling.gamble20Days();
+        Gambling gamble = new Gambling();
+        gamble.gamble20Days();
+        System.out.println(" \n In total 30 days, the player has won " + win + " days and loose " + loose + " days");
+        System.out.println("The total gain amount for 30 Days is $" + profit); // printing the total profit
+        System.out.println("Maximum Amount Won: " + maxWin);
+        System.out.println("Maximum Amount Won On Day " + maxWinDay);
+        System.out.println("Maximum Amount Loss: " + maxLoss);
+        System.out.println("Maximum Amount Loss On Day " + maxLossDay);
     }
 }
+
